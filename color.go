497f9efafcc4ea7192f54da75483f8c9f6c5f3aa
Jacob Barzee <jmbarzee@gmail.com>
2020-11-12 12:57:49 -0700

    Added missing return values to interface definition of Color

--- color.go
+++ color.go
@@ -1,8 +1,9 @@
 package color
 
+// Color is any representation of a color
 type Color interface {
 	// HSL returns a color represented by Hue, Saturation, and Lightness
-	HSL()
+	HSL() HSL
 	// RGB returns a color represented by Red, Green, Blue
-	RGB()
+	RGB() RGB
 }
